9b5b42ae27251fb7fbc5579e2e0a1c827528653d
Jesutoni <ojesutoni@gmail.com>
2024-02-12 09:17:23 +0100

    add files

---
+++ main.py
@@ -0,0 +1,10 @@
+from website import create_app
+
+app = create_app()  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
